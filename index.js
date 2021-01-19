403dd20df0a680328397e1fc511768ef59138529
ish <ihack@DESKTOP-NH907PL.localdomain>
2021-01-18 16:05:14 -0800

    3rd Kata

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ function paperwork(n, m) {
     return n * m;
   }
 }
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+function problem(x){
+  if (typeof x=='number') {
+  
+  return (x*50)+6;
+  } else {
+  return "Error";
+  }
+  }
