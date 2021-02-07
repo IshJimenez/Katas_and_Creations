23c21497c489563ab4df6d63de2a144db3edc1f8
ish <ish@knights.ucf.edu>
2021-02-06 23:04:01 -0800

    Sat Kata

--- index.js
+++ index.js
@@ -45,3 +45,34 @@ function problem(x){
     }
     return sum;
   };
+
+//   Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime.
+
+// Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+// Requirements
+// You can assume you will be given an integer input.
+// You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+// NOTE on performance: There are no fancy optimizations required, 
+// but still the most trivial solutions might time out. Numbers go up to 2^31 (or similar, depends on language version). 
+// Looping all the way up to n, or n/2, will be too slow.
+
+function isPrime(num){
+  let result = true;
+  if (num < 2){
+    return false;
+  } else if (num === 2) {
+    return true;
+  }
+  if (num % 2 === 1){
+    for (let i=2; i<=Math.ceil(Math.sqrt(num)); i++){
+      if(num % i === 0){
+        result = false;
+        break;
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+}
