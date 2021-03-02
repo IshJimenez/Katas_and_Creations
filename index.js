7c8bc0782a20a9b0664b7dae0263ccb04d832765
ish <ish@knights.ucf.edu>
2021-03-01 21:40:23 -0800

    Commit for today descending function

--- index.js
+++ index.js
@@ -297,3 +297,17 @@ function lettersOnly(str) {
 function lettersOnly(str) {
 	return str.match(/[A-Z]|[a-z]/g).join('');
 }
+
+Sort Numbers in Descending Order
+Create a function that takes any nonnegative number as an argument and return it with it's digits in descending order. Descending order is when you sort from highest to lowest.
+
+Examples
+sortDescending(123) ➞ 321
+
+sortDescending(1254859723) ➞ 9875543221
+
+sortDescending(73065) ➞ 76530
+
+function sortDescending(num) {
+	return parseInt(String(num).split('').sort().reverse().join(''))
+}
