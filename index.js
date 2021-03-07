6293f57aa5a842af838f89038cde06a539ddff69
ish <ish@knights.ucf.edu>
2021-03-06 21:57:32 -0800

    Weekend Kata

--- index.js
+++ index.js
@@ -320,3 +320,12 @@ swapcase = function swapcase(str) {
         });
     }
 console.log(swapcase('AaBbc'));
+
+Create a function called that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+If no occurrences can be found, a count of 0 should be returned.
+
+function strCount(str, letter){  
+
+ return str.split('').filter(a => a == letter).length
+}
