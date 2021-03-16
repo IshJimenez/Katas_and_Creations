e237125787440c493d5bb239dbbf9c2fb942d152
ish <ish@knights.ucf.edu>
2021-03-15 23:44:22 -0700

    Kata for today

--- index.js
+++ index.js
@@ -395,3 +395,34 @@ function highAndLow(numbers){
   const arr = numbers.split(" ").map(item => Number(item)).sort((a,b) => b - a)
   return String(arr[0] + " " + String(arr[arr.length - 1]))
 }
+
+Description:
+Encrypt this!
+
+You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+Your message is a string containing space separated words.
+You need to encrypt each word in the message using the following rules:
+The first letter needs to be converted to its ASCII code.
+The second letter needs to be switched with the last letter
+Keepin' it simple: There are no special characters in input.
+
+function encryptThis(text) {
+  let strArr = text.split(' ');
+  let output = [];
+  
+  strArr.forEach(str => {
+    if (str.length === 1) {
+      output.push(str.codeAt(0));
+    } 
+    else {
+      let tempStr = str.split('');
+      tempStr[0] = str.codeAt(0);
+      tempStr[1] = str[str.length - 1];
+      tempStr[str.length - 1] = str[1];
+      output.push(tempStr.join(''));
+    }
+  });
+  
+  return output.join(' ');
+}
