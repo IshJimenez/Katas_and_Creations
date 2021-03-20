553471d9a642ae3d3346c395b1707bf33cd1161a
Ish J <ish@knights.ucf.edu>
2021-03-19 19:35:20 -0700

    kata for today

--- index.js
+++ index.js
@@ -435,3 +435,19 @@ Area of a Triangle
 Write a function that takes the base and height of a triangle and return its area.
 
 const triArea = (base, height) => (base * height) / 2;
+
+Return the First Element in an Array
+Create a function that takes an array containing only numbers and return the first element.
+
+Examples
+getFirstValue([1, 2, 3]) ➞ 1
+
+getFirstValue([80, 5, 100]) ➞ 80
+
+getFirstValue([-500, 0, 50]) ➞ -500
+Notes
+The first element in an array always has an index of 0.
+
+function getFirstValue(arr) {
+	return arr[0];
+}
