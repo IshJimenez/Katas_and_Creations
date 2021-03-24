ee0c28e8fd1ba50bfcebe84117893e094d002ef2
Ish J <ish@knights.ucf.edu>
2021-03-23 19:26:21 -0700

    kata

--- index.js
+++ index.js
@@ -513,3 +513,23 @@ points(38, 8) ➞ 100
 function points(twoPointers, threePointers){
 	return twoPointers * 2 + threePointers * 3;
 }
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      return "alpha";
+      break;
+    case 2:
+      return "beta";
+      break;
+    case 3:
+      return "gamma";
+      break;
+    case 4:
+      return "delta";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
