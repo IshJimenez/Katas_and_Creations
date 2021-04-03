9d32d05f32bc3de55b9ad073f465476f7dbac5f7
Ish J <ish@knights.ucf.edu>
2021-04-02 23:00:37 -0700

    Kata for today

--- index.js
+++ index.js
@@ -682,3 +682,20 @@ return undefined;
 }
 
 abTest(2,2);
+Convert the assignments for a, b, and c to use the *= operator.
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a = a * 5;
+b = 3 * b;
+c = c * 10;
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+// Division example
+a /= 12;
+b /= 4;
+c /= 11;
