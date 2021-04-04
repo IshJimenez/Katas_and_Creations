90dc7fc4ee7488ef06e92adfe3b63b5819c38ef4
Ish J <ish@knights.ucf.edu>
2021-04-03 23:22:27 -0700

    kata for today

--- index.js
+++ index.js
@@ -699,3 +699,7 @@ c *= 10;
 a /= 12;
 b /= 4;
 c /= 11;
+Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+
+I am a "double quoted" string inside "double quotes".
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."
