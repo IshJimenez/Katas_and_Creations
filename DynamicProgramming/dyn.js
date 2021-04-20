35f67c626708f440900b7ecca7e97e160f470333
Ish J <ish@knights.ucf.edu>
2021-04-19 23:26:09 -0700

    Added and solved the next memoized problem

--- DynamicProgramming/dyn.js
+++ DynamicProgramming/dyn.js
@@ -332,3 +332,44 @@ console.log(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
     'eeeee',
     'eeeeee',
 ])) // false
+
+
+// ---------------------countConstruction
+//
+// Write a function `countConstruction(target, wordbank)` that accepts a 
+// target string and an array of strings.
+//
+// The function should return the number of ways that the `target` can be constructed
+// by concatenating elements of the `wordBank` array.
+//
+// You may reuse elements of `wordBank` as many times as needed.
+
+const countConstruction = (target, wordBank, memo={}) => {
+if (target in memo) return memo[target];
+if (target === '') return 1;
+let totalCount = 0;
+
+for (let word of wordBank) {
+    if(target.indexOf(word) === 0) {
+    const numWaysForRest = countConstruction(target.slice(word.length), wordBank, memo)
+    totalCount += numWaysForRest;
+    }
+}
+
+memo[target] = totalCount;
+return totalCount;
+
+}
+
+console.log(countConstruction("purple", ['purp','p','ur','le','purl'])) //2
+console.log(countConstruction("abcdef", ['ab','abc','cd','def','abcd'])) //1
+console.log(countConstruction("skateboard", ['bo','rd','ate','t','ska','sk','boar'])) //0
+console.log(countConstruction("enterapotentpot", ['a','p','ent','enter','ot','o','t'])) //4
+console.log(countConstruction("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    'e',
+    'ee',
+    'eee',
+    'eeee',
+    'eeeee',
+    'eeeeee',
+])) // 0
