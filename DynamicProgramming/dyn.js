8a3e797b7ba4187a2314053f54e70735037a14e6
Ish J <ish@knights.ucf.edu>
2021-04-27 23:41:01 -0700

    Added and solved the fib problem

--- DynamicProgramming/dyn.js
+++ DynamicProgramming/dyn.js
@@ -417,3 +417,31 @@ console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
     'eeeee',
     'eeeeee',
 ])) // Yeah what he said
+
+// ---------------------fib
+//
+// Write a function `fib(n)` that takes in a number as an argument. The function
+// should return the n-th number of the Fibonacci sequenece.
+//
+// The 0th number of the sequence is 0.
+// The 1st number of the sequence is 1.
+//
+// To generate the next number of sequence, we sum the previous two.
+//
+// m:       0,1,2,3,4,5,6,7,8,9, ---
+// fib(n):  0,1,1,2,3,5,8,13,21,34, ---
+
+const fibx = (n) => {
+    const table = Array(n + 1).fill(0);
+    table[1] = 1;
+    for (let i = 0; i <= n; i++){
+        table[i + 1] += table[i]
+        table[i + 2] += table[i]
+    }
+    return table[n];
+}
+
+console.log(fibx(6)); //8
+console.log(fibx(7)); //13
+console.log(fibx(8)); //21
+console.log(fibx(50)); //12586269025
