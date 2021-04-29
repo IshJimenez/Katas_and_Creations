0f2f72e4095927d5ca00f8c9c3d31d923be28b41
Ish J <ish@knights.ucf.edu>
2021-04-29 15:34:07 -0700

    Continued adding to the RPG file

--- RPGBasics/script.js
+++ RPGBasics/script.js
@@ -1,4 +1,5 @@
-// Inside a location
+//  ------------------ Inside a location
+//
 {
     imageSrc: "Image png would go here",
     people: [
@@ -20,28 +21,107 @@
         walls: [ "3x1", "4x1"]
 }
 
-// Camera View
+// ------------------ Camera View
+//
 // Within the wiewpoint the character will be in the middle.
 // The map will move in the opposite direction as the character moves in the map.
-// You will want
 
-// .game-viewport {
-//      overflow: visible;
-// }
+.game-viewport {
+     overflow: visible;
+}
 
 // More like pokemon style where its a smaller view
-// .game-viewport {
-//      overflow: hidden;
-// }
+.game-viewport {
+     overflow: hidden;
+}
 
-// Game Art
+// ------------------ Game Art
 //Natural png asset size 128 * 128
+// STE can be used just be aware of the file size
+
+// The character will be the similar to the camera view
+// Where you have all the images of the pixels and it just moves to
+// show the selected position 
 
 // Scales up pixel art cleanly
+// Wouldnt work for photos
 
-// canvas {
-//     image-rendering: -moz-crisp-edges;
-//     image-rendering: -webkit-crisp-edges;
-//     image-rendering: pixelated;
-//     image-rendering: crisp-edges;
+canvas {
+    image-rendering: -moz-crisp-edges;
+    image-rendering: -webkit-crisp-edges;
+    image-rendering: pixelated;
+    image-rendering: crisp-edges;
+}
+
+//  ------------------ Spritesheet Animation
+//
+// 1 Only show one frame - overflow: hidden
+// 2 Move the spritesheet underneath the crop
+
+@keyframes walkDown {
+   0% {
+     transform: translate3d(0,0,0)
+}
+  100% {
+     transform: translate3d(-100%,0,0)
+}
+}
+//
+// 3 Complete this animation in `steps` jumps
+// steps would transform from one frame to another
+// In the example below it has 4 since it only had 4 steps
+// .person-walking-down {
+//    animation:
+//      walkDown 0.5s steps(4) infinite;
 // }
+
+//  ------------------ Turn Base Battles
+//
+// State of combatants
+// belongsToTeam number would place them in their starting locatopn
+[
+    {
+        id: 1,
+        name: "Pat",
+        maxHP: 100,
+        hp: 100,
+        belongsToTeam: "one",
+        dangerMeter: 100
+    },
+    {
+        id: 2,
+        name: "Enemy Dude",
+        maxHP: 80,
+        hp: 4,
+        belongsToTeam: "two",
+    }     
+]
+
+//  ------------------ Actions
+//
+
+"action_slice": {
+    name: "Slice",
+    description: "Offensive move",
+    iconType: "attack",
+
+    successEvents(caster, targets) {
+        return [
+            {
+                type: "PLAY_ANIMATION",
+                animationId: "Slice",
+                onCharacters: ["enemy1"]
+            },
+            {
+                type: "CHANGE_HP",
+                relativeChange: -20,
+                onCharacters: ["enemy1"]
+            }
+        ]
+    },
+
+    shouldFail(caster, targets) {
+        // Logic goes in this section
+        return false
+    }
+}
