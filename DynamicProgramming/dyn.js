96264214e0333f641378ba9505843dc5e1bac811
Ish J <ish@knights.ucf.edu>
2021-05-06 17:19:46 -0700

    Added tabulation recipe and next problem

--- DynamicProgramming/dyn.js
+++ DynamicProgramming/dyn.js
@@ -479,3 +479,26 @@ console.log(gridTraveler2(2, 3)); //3
 console.log(gridTraveler2(3, 2)); //3
 console.log(gridTraveler2(3, 3)); //6
 console.log(gridTraveler2(18, 18)); //2333606220
+
+// ---------------------Tabulation Recipe
+//
+//  1) Visualize the problem as a table
+//  2) Size the table based on the inputs
+//  3) Intialize the table with default values
+//  4) Seed the trivial answer into the table
+//  5) Iterate through the table
+//  6) Fill further positions based on the current position
+
+
+// ---------------------canSum
+//
+// Write a function `canSum(targetSum, numbers)` that takes in a 
+// targetSum and an array of numbers as arguments.
+//
+// The function should return a boolean indicating whether or not it
+// is possible to generate the targetSum using numbers from the array.
+//
+// You may use an element of the array as many times as needed.
+//
+// You may assume that all input numbers are nonnegative.
+//
