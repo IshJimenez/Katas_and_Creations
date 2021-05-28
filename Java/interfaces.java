6d26beef3a1eb7960d175084688b6000c78d1d73
Ish J <ish@knights.ucf.edu>
2021-05-28 14:12:30 -0700

    Added info to interfaces

--- Java/interfaces.java
+++ Java/interfaces.java
@@ -17,7 +17,7 @@ for every interface method, or be an abstract class.
 
 Interface methods do not have implementations and interface avriables are static, so no states or behavior are inherited.
 
-Said to have type inheritance. A class that implements an interface is polymorphicaly considered an instance of the interface.
+Said to have type inheritance. A class that implements an interface is polymorphically considered an instance of the interface.
 
 Name interfaces are adjectives or verbs.
 
@@ -67,6 +67,78 @@ class Dog extends Animal implements Domesticated {
 	}
 }
 
+The animal class defines eat method, and the Domesticated interface dictates that an implementing class must complete the doWork method.
+
+Also the wolf class, extends Animal, and Dog class both extends Animal, and implements Domesticated.
+
+Wolf and Dog both override eat. Because Dog implements the Domesticated interface, it also implements the doWork method.
+
+We use the poolymorphism to refer to the Dog object with a Domesticated type reference variable.
+
+We can only use this variable to call the doWork() method we cant call the eat() method from a Demoesticated type reference variable, because the 
+eat() method isnt delcared in the interface.
+
+Since we are using a Domesticated interface type, this code can now support any Domesticated object 
+A houseAlien object, a Horse object.
+
+They all gurantee to have an implementation for the doWork() method.
+
+And as long as we imlement the Domesticated interface we can be assured that future changes to the "doWork" implemntation in the Dog class will never
+break the code that invokes it.
+
 Java 8 Interfaces
 
-public interface Doable
+public interface Doable {
+    void doAthing()
+    void doAnotherThing()  == This would need to be implemented below 
+}
+public class Task implements Doable {
+    public void doAThing() {
+        System.out.println("Doneeee");
+    }
+}
+public class Chore implements Doable {
+    static int choresDone = 0;
+
+    public void doAThing() {
+        choresDone++;
+    }
+}
+
+Public facing interfaces (APIs) were difficult to update or change
+
+Default methods: Interface methods that are implemented in the interface
+
+Implementing classes are not required to implement default methods, but they can override them.
+
+public interface Doable {
+    default void doAthing() {
+        int things = 0;
+        System.out.println("Doneeee");
+    }
+    void checkTasks();
+}
+
+Interfaces can have static methods with fefinitions
+
+Static methods can be called w/o creating an instance of the class (or interface) first
+
+Static methods an only call other static methods directly. They must use local reference variables to call non-static methods.
+
+Staic interface methods cannot call other interface methods that are not also static or default.
+
+Multiple Inheritance
+
+If two interfaces contain default methods with the same name, a class that implements both will have a conflict.
+
+The super keywoord can be used to specify which interface implementation to use.
+
+Doesticated.super.doWork();
+Worker.super.doWork();
+
+Abstract Classes
+
+Can have instance variables that are not alwalys public static final
+
+Can have methods that are not public.
+
