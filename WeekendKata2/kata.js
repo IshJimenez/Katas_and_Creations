bcf72fce7fb79580bdb86503273f3f0c73425089
Ish J <ish@knights.ucf.edu>
2021-06-24 17:19:20 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -327,3 +327,13 @@ else {
 
 // display the result
 console.log("The largest number is " + largest);
+
+Is this a triangle?
+Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a,b,c) {
+  const sides = [a, b, c].sort();
+  return ((sides[0] + sides[1]) > sides[2]);
+}
