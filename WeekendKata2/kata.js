53cbe8905531c34997b34e9d970c171abff8fc5b
Ish J <ish@knights.ucf.edu>
2021-06-27 17:30:58 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -365,3 +365,15 @@ There will always be only one integer that appears an odd number of times.
 function findOdd(arr) {
   return arr.find((item) => arr.filter(el => el == item).length % 2)
 }
+
+Write a JavaScript program to find which 1st January is being a Sunday between 2014 and 2050.
+
+The getDay() method is used to get the day of the week for the specified date according to local time, where 0 represents Sunday. 
+The value returned by getDay() is an integer corresponding to the day of the week: 0 for Sunday, 1 for Monday, 2 for Tuesday, and so on.
+
+for (var year = 2014; year <= 2050; year++)
+    {
+    var d = new Date(year, 0, 1);
+    if ( d.getDay() === 0 )
+        console.log("1st January is being a Sunday  "+year);
+    };
