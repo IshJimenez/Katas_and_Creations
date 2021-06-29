ef24c416344d9f1b55d736b2f854f51f65ea822f
Ish J <ish@knights.ucf.edu>
2021-06-29 13:49:51 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -386,3 +386,14 @@ for (var year = 2014; year <= 2050; year++)
     var s = (side1 + side2 + side3)/2;
     var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
     console.log(area);
+
+Write a JavaScript function that reverse a number.
+    Sample Data and output:
+    Example x = 32243;
+    Expected Output: 34223
+
+function reverse_a_number(n){
+	    n = n + "";
+	    return n.split("").reverse().join("");
+    }
+    console.log(reverse_a_number(32243));
