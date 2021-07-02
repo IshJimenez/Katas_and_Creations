edf4deb030bad81f364977bb02fa7113628e437b
Ish J <ish@knights.ucf.edu>
2021-07-01 17:11:20 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -422,4 +422,30 @@ The getTime() method is used to get the numeric value corresponding to the time 
 
 The Math.ceil() function is used to get the smallest integer greater than or equal to a given number.
 
+Write a JavaScript function to convert an amount to coins.
 
+Sample function : amountTocoins(46, [25, 10, 5, 2, 1])
+Here 46 is the amount. and 25, 10, 5, 2, 1 are coins.
+Output : 25, 10, 10, 1
+
+function amountTocoins(amount, coins) 
+{
+ if (amount === 0) 
+  {
+     return [];
+   } 
+ else
+   {
+     if (amount >= coins[0]) 
+       {
+        left = (amount - coins[0]);
+        return [coins[0]].concat( amountTocoins(left, coins) );
+        } 
+      else
+        {
+         coins.shift();
+         return amountTocoins(amount, coins);
+        }
+    }
+} 
+console.log(amountTocoins(46, [25, 10, 5, 2,1]));
