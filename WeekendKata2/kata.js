84786858dbb06b2b4adc9bcbce50bf89ba07c600
Ish J <ish@knights.ucf.edu>
2021-07-03 14:15:53 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -465,3 +465,32 @@ text += char_list.charAt(Math.floor(Math.random() * char_list.length));
 return text;
 }
 console.log(makeid(8));
+
+Write a JavaScript function to find the first not repeated character.
+
+Sample arguments : 'abacddbec'
+Expected output : 'e'
+
+function find_FirstNotRepeatedChar(str) {
+  var arra1 = str.split('');
+  var result = '';
+  var ctr = 0;
+ 
+  for (var x = 0; x < arra1.length; x++) {
+    ctr = 0;
+ 
+    for (var y = 0; y < arra1.length; y++) 
+    {
+      if (arra1[x] === arra1[y]) {
+        ctr+= 1;
+      }
+    }
+ 
+    if (ctr < 2) {
+      result = arra1[x];
+      break;
+    }
+  }
+  return result;
+}
+console.log(find_FirstNotRepeatedChar('abacddbec'));
