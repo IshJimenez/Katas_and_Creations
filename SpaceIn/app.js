f1e5ee6a0eea1c7b24ae15ecc9b3d37043500cb2
Ish J <ish@knights.ucf.edu>
2021-07-06 09:18:38 -0700

    Adding to SpaceIn ..started game logic and enemies  spawn and moves down, character can move L and R and edges have been defined

---
+++ SpaceIn/app.js
@@ -0,0 +1,79 @@
+const frid = document.querySelector('.frid')
+let currentShooterIndex = 202
+let width = 15
+let direction = 1
+let invadersId
+let goingRight = true
+
+for (let i = 0; i < 225; i++) {
+    const square = document.createElement('div')
+    frid.appendChild(square)
+}
+
+const squares = Array.from(document.querySelectorAll('.frid div'))
+
+const alienIn = [
+    0,1,2,3,4,5,6,7,8,9,15,
+    16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
+]
+
+function draw() {
+    for (let i = 0; i < alienIn.length; i++) {
+        squares[alienIn[i]].classList.add('invader')
+    }
+}
+
+draw()
+
+function remove() {
+    for (let i = 0; i < alienIn.length; i++) {
+        squares[alienIn[i]].classList.remove('invader')
+    }
+}
+
+squares[currentShooterIndex].classList.add('shooter')
+
+function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch(e.key) {
+        case 'ArrowLeft':
+            if (currentShooterIndex % width !== 0) currentShooterIndex -=1
+            break;
+        case 'ArrowRight':
+            if (currentShooterIndex % width < width -1) currentShooterIndex +=1
+            break;
+    }
+    squares[currentShooterIndex].classList.add('shooter')
+}
+
+document.addEventListener('keydown', moveShooter)
+
+function moveAlienIn() {
+    const leftEdge = alienIn[0] % width === 0
+    const rightEdge = alienIn[alienIn.length - 1] % width === width - 1
+    remove()
+
+    if (rightEdge && goingRight) {
+        for (let i = 0; i < alienIn.length; i++) {
+            alienIn[i] += width +1
+            direction = -1
+            goingRight = false
+        }
+    }
+
+    if (leftEdge && !goingRight) {
+        for (let i = 0; i < alienIn.length; i++) {
+            alienIn[i] += width -1
+            direction = 1
+            goingRight = true
+        }
+    }
+
+    for (let i = 0; i < alienIn.length; i++) {
+    alienIn[i] += direction
+    }
+    draw()
+}
+
+invadersId = setInterval(moveAlienIn, 500)
