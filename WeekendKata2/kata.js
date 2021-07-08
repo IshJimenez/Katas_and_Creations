839d721d2822b86d5dca4e299b64674312c47702
Ish J <ish@knights.ucf.edu>
2021-07-07 17:48:16 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -516,3 +516,29 @@ function Char_Counts(str1) {
   return uchars;
   }
   console.log(Char_Counts("The quick brown fox jumps over the lazy dog"));
+
+
+Write a JavaScript function which accepts an argument and returns the type.
+
+Note : There are six possible values that typeof returns: object, boolean, function, number, string, and undefined.
+
+function detect_data_type(value)
+{
+var dtypes = [Function, RegExp, Number, String, Boolean, Object], x, len;
+    
+if (typeof value === "object" || typeof value === "function") 
+    {
+     for (x = 0, len = dtypes.length; x < len; x++) 
+     {
+            if (value instanceof dtypes[x])
+            {
+                return dtypes[x];
+            }
+      }
+    }
+    
+    return typeof value;
+}
+console.log(detect_data_type(12));
+console.log(detect_data_type('w3resource'));
+console.log(detect_data_type(false));
