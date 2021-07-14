a46a1da98485ec68e2a670b64b98699bbf449055
Ish J <ish@knights.ucf.edu>
2021-07-14 16:13:02 -0700

    Spawn Bario and the first level environment and  bad dudes. Started working on movement for Bario

--- Bario/game.js
+++ Bario/game.js
@@ -1,28 +1,85 @@
 kaboom({
     global: true,
     fullscreen: true,
-    scale: 1,
+    scale: 2,
     debug: true,
     clearColor: [0, 0, 0, 1],
 })
 
-
-loadRoot('https://imgur.com/')
-loadSprite('coin', 'wbKxhcd.png')
-loadSprite('badDude1', 'KPO3fR9.png')
-loadSprite('brick', 'pogC9x5.png')
-loadSprite('block', 'bdrLpi6.png')
-loadSprite('bario', 'Wb1qfhK.png')
-loadSprite('mushroom', '0wMd92p.png')
-loadSprite('surprise', 'gesQ1KP.png')
-loadSprite('unboxed', 'bdrLpi6.png')
-loadSprite('pipe-tleft', 'ReTPiWY.png')
-loadSprite('pipe-tright', 'hj2GK4n.png')
-loadSprite('pipe-bleft', 'c1cYSbt.png')
-loadSprite('pipe-bright', 'nqQ79eI.png')
+loadSprite('coin', 'aCoin.png')
+loadSprite('badDude1', 'aBadDude1.png')
+loadSprite('brick', 'aBrick.png')
+loadSprite('block', 'aBlock.png')
+loadSprite('bario', 'aBario.png')
+loadSprite('mushroom', 'aMushroom.png')
+loadSprite('surprise', 'aSurprise.png')
+loadSprite('box', 'aBox.png')
+loadSprite('tleft', 'aTleft.png')
+loadSprite('tright', 'aTRight.png')
+loadSprite('bleft', 'aBLeft.png')
+loadSprite('bright', 'aBRight.png')
 
 scene("game", () => {
     layers(['bg', 'obj', 'ui'], 'obj')
+
+const map = [
+    '                                                             ',
+    '                                               %             ',
+    '                                                             ',
+    '                                                             ',
+    '                                                             ',
+    '                                                             ',
+    '                                                             ',
+    '                                                             ',
+    '       %  =*=%=                                               ',
+    '                                                             ',
+    '                                                -+           ',
+    '                                       ^  ^     ()           ',
+    '==================================================    =======',
+]
+
+const levelCfg = {
+    width:  20,
+    height: 20,
+    '=': [sprite('block'), solid()],
+    '$': [sprite('coin')],
+    '%': [sprite('surprise'), solid(), 'coin-surprise'],
+    '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
+    '}': [sprite('box'), solid()],
+    '(': [sprite('bleft'), solid(), scale(0.5)],
+    ')': [sprite('bright'), solid(), scale(0.5)],
+    '-': [sprite('tleft'), solid(), scale(0.5)],
+    '+': [sprite('tright'), solid(), scale(0.5)],
+    '^': [sprite('badDude1', solid())],
+    '#': [sprite('mushroom', solid())],
+
+}
+
+const gameLevel = addLevel(map, levelCfg)
+
+    const scoreLable = add([
+        text('test'),
+        pos(30,6),
+        layer('ui'),
+        {
+            value: 'test',
+        }
+    ])
+
+    add([text('level ' + 'test', pos(4,6))])
+
+    const player = add([
+        sprite('bario'), solid(),
+        pos(30, 0),
+        body(),
+        origin('bot')
+    ])
+
+    const MOVE_SPEED = 120
+
+    keyDown('left', () => {
+        player.move(-MOVE_SPEED, 0)
+    })
 })
 
 start("game")
