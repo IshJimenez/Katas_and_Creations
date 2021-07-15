bdb2cad7fca42debfb75c401af0b825b4b06d088
Ish J <ish@knights.ucf.edu>
2021-07-15 14:37:24 -0700

    Enemies move, and bario can now move and he can headbutt boxes and become big

--- Bario/game.js
+++ Bario/game.js
@@ -6,6 +6,12 @@ kaboom({
     clearColor: [0, 0, 0, 1],
 })
 
+const MOVE_SPEED = 120
+const JUMP_FORCE = 360
+const BIG_JUMP_FORCE = JUMP_FORCE * 1.5
+let CURRENT_JUMP_FORCE = JUMP_FORCE
+
+
 loadSprite('coin', 'aCoin.png')
 loadSprite('badDude1', 'aBadDude1.png')
 loadSprite('brick', 'aBrick.png')
@@ -33,7 +39,7 @@ const map = [
     '                                                             ',
     '       %  =*=%=                                               ',
     '                                                             ',
-    '                                                -+           ',
+    '                    = =                         -+           ',
     '                                       ^  ^     ()           ',
     '==================================================    =======',
 ]
@@ -42,7 +48,7 @@ const levelCfg = {
     width:  20,
     height: 20,
     '=': [sprite('block'), solid()],
-    '$': [sprite('coin')],
+    '$': [sprite('coin'), 'coin'],
     '%': [sprite('surprise'), solid(), 'coin-surprise'],
     '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
     '}': [sprite('box'), solid()],
@@ -50,8 +56,8 @@ const levelCfg = {
     ')': [sprite('bright'), solid(), scale(0.5)],
     '-': [sprite('tleft'), solid(), scale(0.5)],
     '+': [sprite('tright'), solid(), scale(0.5)],
-    '^': [sprite('badDude1', solid())],
-    '#': [sprite('mushroom', solid())],
+    '^': [sprite('badDude1'), solid(), 'dangerous'],
+    '#': [sprite('mushroom'), solid(), 'mushroom', body()],
 
 }
 
@@ -62,24 +68,104 @@ const gameLevel = addLevel(map, levelCfg)
         pos(30,6),
         layer('ui'),
         {
-            value: 'test',
+            value: 'Ramon and Morgan Kissed :-)',
         }
-    ])
+])
 
-    add([text('level ' + 'test', pos(4,6))])
+add([text('level ' + 'test', pos(4,6))])
 
-    const player = add([
+function big() {
+        let timer = 0
+        let isBig = false
+        return {
+            update() {
+            if (isBig) {
+                CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+                timer -= dt() 
+            if (timer <= 0) {
+                this.smallify()
+        }
+        }
+        },
+            isBig() {
+            return isBig
+        },
+            smallify() {
+            this.scale = vec2(1)
+            CURRENT_JUMP_FORCE = JUMP_FORCE
+            timer = 0
+            isBig = false
+        },
+            biggify(time) {
+            this.scale = vec2(2)
+            timer = time
+            isBig = true
+        }
+        }
+}
+
+const player = add([
         sprite('bario'), solid(),
         pos(30, 0),
         body(),
+        big(),
         origin('bot')
-    ])
+])
 
-    const MOVE_SPEED = 120
+action('mushroom', (m) => {
+        m.move(20, 0)
+    })
+
+player.on("headbump", (obj) => {
+        if (obj.is('coin-surprise')) {
+            gameLevel.spawn('$', obj.gridPos.sub(0, 1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0,0))
+        }
+        if (obj.is('mushroom-surprise')) {
+            gameLevel.spawn('#', obj.gridPos.sub(0, 1))
+            destroy(obj)
+            gameLevel.spawn('}', obj.gridPos.sub(0,0))
+        }   
+})
+
+player.collides('mushroom', (m) => {
+        destroy(m)
+        player.biggify(6)
+})
+
+player.collides('coin', (c) => {
+        destroy(c)
+        scoreLable.value++
+        scoreLable.text = scoreLable.value
+
+})
+
+const ENEMY_SPEED = 20
+
+action('dangerous', (d) => {
+        d.move(-ENEMY_SPEED, 0)
+})
+
+player.collides('dangerous', (d) => {
+    go('lose', { score: scoreLable.value})
+})
 
     keyDown('left', () => {
         player.move(-MOVE_SPEED, 0)
     })
+    keyDown('right', () => {
+        player.move(MOVE_SPEED, 0)
+    })
+    keyDown('space', () => {
+        if (player.grounded()) {
+            player.jump(CURRENT_JUMP_FORCE)
+    }
+    })
+})
+
+scene('lose', ({ score }) => {
+    add([text(score, 32), origin('center'), pos(width()/2, height()/ 2)])
 })
 
 start("game")
