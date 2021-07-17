f02226667eb032f7330253d0596590cee2992fcf
Ish J <ish@knights.ucf.edu>
2021-07-17 14:17:27 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -581,3 +581,26 @@ function midrange(a, b, c) {
   let max = Math.max(a, b, c);
   return (min + max) / 2;
 }
+
+Create a function that will receive two arrays and will return an array with elements that are in the first array but not in the second
+
+var ar1 = [1, 2, 3, 10, 5, 3, 14];
+var ar2 = [-1, 4, 5, 6, 14];
+
+var ar = mergeLeft(ar1, ar2);
+println(ar);
+
+function mergeLeft(ar1, ar2)
+{
+    var ar = [];
+    
+    for(let el of ar1)
+    {
+        if (!ar2.includes(el))
+        {
+            ar.push(el);
+        }
+    }
+
+    return ar;
+}
