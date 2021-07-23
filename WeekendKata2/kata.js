0d96207a78bb5f11281a233d117efabde3591e7d
Ish J <ish@knights.ucf.edu>
2021-07-22 17:55:24 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -651,3 +651,15 @@ function isPrime(n)
     
     return true;
 }
+
+Rotate an array to the right 1 position
+
+var ar = [1, 2, 3];
+rotateRight(ar);
+println(ar);
+
+function rotateRight(ar)
+{
+    var last = ar.pop();
+    ar.unshift(last);
+}
