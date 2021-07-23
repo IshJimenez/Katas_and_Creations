4e0898218c0ec899b6736ec27632010b5a193728
Ish J <ish@knights.ucf.edu>
2021-07-23 08:21:12 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -663,3 +663,21 @@ function rotateRight(ar)
     var last = ar.pop();
     ar.unshift(last);
 }
+
+Reverse an array
+
+var ar = [1, 2, 3];
+var ar2 = reverseArray(ar);
+println(ar2);
+
+function reverseArray(ar)
+{
+    var ar2 = [];
+    
+    for(var i = ar.length - 1; i >= 0; i--)
+    {
+        ar2.push(ar[i]);
+    }
+    
+    return ar2;
+}
