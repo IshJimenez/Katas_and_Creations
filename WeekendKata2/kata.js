ec956652396e548dadeec72b07b91b964d9e5284
Ish J <ish@knights.ucf.edu>
2021-07-25 12:21:45 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -701,3 +701,27 @@ var ar = [1, 3, 9, 15, 90];
 var avg = averageArray(ar);
 
 println("Average: ", avg);
+
+Create a function that receives an array of numbers and returns an array containing only the positive numbers
+
+function getPositives(ar)
+{
+    var ar2 = [];
+    
+    for(var i = 0; i < ar.length; i++)
+    {
+        var el = ar[i];
+        
+        if (el >= 0)
+        {
+            ar2.push(el);
+        }
+    }
+    
+    return ar2;
+}
+
+var ar = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+var ar2 = getPositives(ar);
+
+println(ar2);
