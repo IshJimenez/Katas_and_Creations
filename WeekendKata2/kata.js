960aff1c48f9161a395a5fb9e7ef0b5dd7405267
Ish J <ish@knights.ucf.edu>
2021-07-27 17:09:48 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -725,3 +725,24 @@ var ar = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
 var ar2 = getPositives(ar);
 
 println(ar2);
+
+Find the maximum number in an array of numbers
+
+function findMax(ar)
+{
+    var max = ar[0];
+    
+    for(var i = 0; i < ar.length; i++)
+    {
+        if (ar[i] > max)
+        {
+            max = ar[i];
+        }
+    }
+    
+    return max;
+}
+
+var ar = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+var max = findMax(ar);
+println("Max: ", max);
