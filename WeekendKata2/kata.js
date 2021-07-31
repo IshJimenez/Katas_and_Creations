66fc62facafdc812315ec94ace246a43462223c9
Ish J <ish@knights.ucf.edu>
2021-07-31 15:04:03 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -811,3 +811,29 @@ function sumDigits(n)
 
 var sum = sumDigits(1235231);
 println("Sum: ", sum);
+
+Create a function that will merge two arrays and return the result as a new array
+
+
+var ar1 = [1, 2, 3];
+var ar2 = [4, 5, 6];
+
+var ar = mergeArrays(ar1, ar2);
+println(ar);
+
+function mergeArrays(ar1, ar2)
+{
+   var ar = [];
+   
+   for(let el of ar1)
+   {
+       ar.push(el);
+   }
+   
+   for(let el of ar2)
+   {
+       ar.push(el);
+   }
+   
+   return ar;
+}
