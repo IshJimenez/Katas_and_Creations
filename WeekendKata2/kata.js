1b126794597eb78be91afee49701a4c8148cfe1c
Ish J <ish@knights.ucf.edu>
2021-08-01 15:35:22 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -837,3 +837,36 @@ function mergeArrays(ar1, ar2)
    
    return ar;
 }
+
+Create a function that will receive two arrays of numbers as arguments and return an array composed of all the numbers that are either in the first 
+array or second array but not in both
+
+var ar1 = [1, 2, 3, 10, 5, 3, 14];
+var ar2 = [1, 4, 5, 6, 14];
+
+var ar = mergeExclusive(ar1, ar2);
+println(ar);
+
+function mergeExclusive(ar1, ar2)
+{
+    var ar = [];
+    
+    for(let el of ar1)
+    {
+        if (!ar2.includes(el))
+        {
+            ar.push(el);
+        }
+    }
+    
+    for(let el of ar2)
+    {
+        if (!ar1.includes(el))
+        {
+            ar.push(el);
+        }
+    }
+    
+    return ar;
+}
+
