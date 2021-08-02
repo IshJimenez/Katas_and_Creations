f454a67cba29e2a12b042a381277b4ec17c38887
Ish J <ish@knights.ucf.edu>
2021-08-02 16:12:35 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -870,3 +870,33 @@ function mergeExclusive(ar1, ar2)
     return ar;
 }
 
+Create a function that will receive an array of numbers as argument and will return a new array with distinct elements
+
+var ar = getDistinctElements([1, 2, 3, 6, -1, 2, 9, 7, 10, -1, 100]);
+println(ar);
+
+function getDistinctElements(ar)
+{
+    var ar2 = [];
+    
+    for(let i = 0; i < ar.length; i++)
+    {
+        if (!isInArray(ar[i], ar2))
+        {
+            ar2.push(ar[i]);
+        }
+    }
+    
+    return ar2;
+}
+
+function isInArray(n, ar)
+{
+    for(let i = 0; i < ar.length; i++)
+    {
+        if (ar[i] === n)
+            return true;
+    }
+    
+    return false;
+}
