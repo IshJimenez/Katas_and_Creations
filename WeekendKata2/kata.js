a772b0459ba6243c35aa4703dbd884a6d7e9d5b0
Ish J <ish@knights.ucf.edu>
2021-08-03 16:22:31 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -900,3 +900,21 @@ function isInArray(n, ar)
     
     return false;
 }
+
+Calculate the sum of numbers received in a comma delimited string
+
+println(sumCSV("1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9"));
+
+function sumCSV(s)
+{
+    var ar = s.split(",");
+    
+    var sum = 0;
+    
+    for(var n of ar)
+    {
+        sum += parseFloat(n);
+    }
+    
+    return sum;
+}
