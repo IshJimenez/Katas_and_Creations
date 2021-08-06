c746e39402b408c744763c613c344e46c2f4adb8
Ish J <ish@knights.ucf.edu>
2021-08-06 15:41:47 -0700

    Kata for today f.2

--- WeekendKata2/basics.js
+++ WeekendKata2/basics.js
@@ -22,3 +22,13 @@ function howManySeconds(hours) {
 	const sec = min * 60
 	return sec
 }
+
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+function lessThanOrEqualToZero(num) {
+	if (num <= 0) {
+		return true
+	} else {
+		return false
+	}
+}
