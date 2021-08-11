86d98dab3d57e5dace6bae7344d2b1eb6d50bf71
Ish J <ish@knights.ucf.edu>
2021-08-11 06:36:26 -0700

    Kata for today f.3

--- WeekendKata2/basics.js
+++ WeekendKata2/basics.js
@@ -32,3 +32,11 @@ function lessThanOrEqualToZero(num) {
 		return false
 	}
 }
+
+Write a function toCase that takes a string and returns that string in lowercase and uppercase with - as delimiter.
+
+Example: toCase('Mthatha') should return 'mthatha-MTHATHA'.
+
+function toCase(text) {
+	return text.toLowerCase() + '-' + text.toUpperCase();
+  }
