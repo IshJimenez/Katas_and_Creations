51d428e6546dc29a526c0b076a1de2406868ca5b
Ish J <ish@knights.ucf.edu>
2021-08-21 16:39:52 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -990,3 +990,36 @@ function FirstFactorial(number) {
    
 // keep this function call here 
 console.log(FirstFactorial(readline()));
+
+Bracket Matcher
+Have the function BracketMatcher(str) take the str parameter being passed and return 1 if the brackets are correctly matched and each one is accounted for. Otherwise return 0. For example: if str is "(hello (world))", then the output should be 1, but if str is "((hello (world))" the the output should be 0 because the brackets do not correctly match up. Only "(" and ")" will be used as brackets. If str contains no brackets return 1.
+Examples
+Input: "(coder)(byte))"
+Output: 0
+Input: "(c(oder)) b(yte)"
+Output: 1
+
+
+function BracketMatcher(str) { 
+  var leftCount = 0;
+  var rightCount = 0;
+  for (var i = 0; i < str.length; i++) {
+      if (str[i] == "(") {
+          leftCount++;
+      } else if (str[i] == ")") {
+          rightCount++;
+      }
+      if (rightCount > leftCount) {
+          return 0;
+      }
+  }
+
+  if (leftCount == rightCount) {
+      return 1;
+  } else {
+      return 0;
+  }
+}
+ 
+// keep this function call here 
+console.log(BracketMatcher(readline()));
