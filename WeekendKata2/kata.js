437004169e0edea8acdfecbdc7ebe0caeb4652c1
Ish J <ish@knights.ucf.edu>
2021-08-22 17:46:19 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -1023,3 +1023,38 @@ function BracketMatcher(str) {
  
 // keep this function call here 
 console.log(BracketMatcher(readline()));
+
+Find Intersection
+Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.
+Examples
+Input: ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+Output: 1,4,13
+Input: ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]
+Output: 1,9,10
+
+function FindIntersection(strArr) { 
+
+  // code goes here  
+  
+    let newArr = [];
+  let el1 = strArr[0].split(', ');
+  let el2 = strArr[1].split(', ');
+  
+
+  // code goes here  
+  for(let i = 0; i < el1.length; i++ ) {
+    if (el2.includes(el1[i])) {
+      newArr.push(el1[i]);
+    }
+  }
+if (newArr.length === 0) {
+  return false;
+} 
+else {
+  return newArr.toString();
+}
+
+}
+   
+// keep this function call here 
+console.log(FindIntersection(readline()));
