4c5f0ea8717ed939f22e9d83d575b59f5a35c7c8
Ish J <ish@knights.ucf.edu>
2021-08-23 23:07:01 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -1058,3 +1058,26 @@ else {
    
 // keep this function call here 
 console.log(FindIntersection(readline()));
+
+Longest Word
+Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
+Examples
+Input: "fun&!! time"
+Output: time
+Input: "I love dogs"
+Output: love
+
+function LongestWord(sent) { 
+
+  let arr = sent.match(/[a-z]+/gi);
+
+  let sorted = arr.sort(function(a, b) {
+    return b.length - a.length;
+  });
+
+  return sorted[0];
+         
+}
+   
+// keep this function call here 
+LongestWord(readline());
