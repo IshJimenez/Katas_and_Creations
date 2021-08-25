c86427bc4216a687a5c6c8f5b7fbc3ca6d56fae9
Ish J <ish@knights.ucf.edu>
2021-08-24 20:20:30 -0700

    Kata for today

--- WeekendKata2/kata.js
+++ WeekendKata2/kata.js
@@ -1081,3 +1081,24 @@ function LongestWord(sent) {
    
 // keep this function call here 
 LongestWord(readline());
+
+First Reverse
+Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+Examples
+Input: "coderbyte"
+Output: etybredoc
+Input: "I Love Potatoes"
+Output: seotatoP evoL I
+
+function FirstReverse(str) { 
+  var temp = '';
+  for (var i = 0; i<str.length; i++) {
+      temp = str[i] + temp;
+  }
+  // code goes here  
+  return temp; 
+         
+}
+   
+// keep this function call here 
+FirstReverse(readline());
