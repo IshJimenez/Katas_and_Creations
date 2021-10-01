2d0d26552c4039504746faec3491af50175502fa
Ish J <ish@knights.ucf.edu>
2021-09-30 18:46:30 -0700

    Added a background(need to expand), music and enemy towwers move towards character and points are added and show on the game over screen

--- Jumper/game.js
+++ Jumper/game.js
@@ -4,32 +4,110 @@ kaboom({
     fullscreen: true,
     scale: 2,
     debug: true,
-    clearColor: [0, 0, 0, 1],
+    clearColor: [51, 151, 255]
 })
 
 // load assets
 loadSprite('bean', 'bean.png');
 // loadSprite('pep', 'pep.gif');
+loadSprite('elom', 'elom.gif');
 
-const bean = add([
-    sprite("bean"),
-    pos(80, 40),
-    area(),
-    body(),
+loadSound('mainFloor', 'Metre.mp3')
+
+scene("game", ({score}) => {
+    layers(['bg', 'obj', 'ui'], 'obj')
+
+    add([sprite('elom'), layer('bg')])
+
+    const music = play('mainFloor', {
+        loop: true
+    })
+
+
+    const bean = add([
+        sprite("bean"),
+        pos(80, 40),
+        area(),
+        body(),
+        music.play()
+    ])
+    
+    // Platform for our dear bean
+    const floor = add([
+        rect(width(), 48),
+        pos(0, height() - 48),
+        outline(4),
+        area(),
+        solid(),
+        color(127, 200, 255),
+    ])
+    
+    function spwanTree() {
+            // Add tree
+            add([
+                rect(48, rand(24, 64)),
+                area(),
+                outline(4),
+                pos(width(), height() - 48),
+                origin("botleft"),
+                color(255, 180, 255),
+                move(LEFT, 240),
+                "tree",
+            ]);
+            wait(rand(0.6, 1.5), () => {
+                spwanTree();
+            })
+            }
+    spwanTree();
+    
+    // keyPress("space", () => {
+    //     if (bean.grounded()) {
+    //     bean.jump()
+    //     score++;
+    //     scoreLabel.text = score;
+    //     }
+    // })
+
+    mouseClick(()   => {
+        if (bean.grounded()) {
+        bean.jump()
+        score++;
+        scoreLabel.text = score;
+        }
+    })
+    
+    bean.collides("tree", () => {
+        addKaboom(bean.pos);
+        shake()
+        music.stop();
+        go("lost", {score})
+    })
+
+// let score = 0;
+const scoreLabel = add([
+    text(score),
+    pos(24,24),
+    layer('ui'),
+    // {
+    //     value: score,
+    // }
 ])
 
-keyPress("space", () => {
-    bean.jump()
+// action(() => {
+//     score++;
+//     scoreLabel.text = score;
+// });
+
 })
 
-add([
-    rect(width(), 48),
-    pos(0, height() - 48),
-    outline(4),
-    area(),
-    solid(),
-    color(127, 200, 255),
-])
+scene("lost", ({score}) => {
+    add([text("NOT a winner", 100), origin('center'), pos(width()/2, height()/ 4)])
+    add([text("Click to lose again" , 32), origin('center'), pos(width()/2, height()/ 2)])
+    add([text("Score: " + score , 32), origin('bot'), pos(width()/2, height()/ 1.25)])
 
+    // go back t}o game with space is pressed
+    // keyPress("space", () => go("game"));
+    mouseClick(() => window.location.reload());
+})
 
-
+go("game", {score: 0})
