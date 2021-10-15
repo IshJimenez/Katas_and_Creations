30ea909d48aee0f20bc7fbf90a1b0bd3942b63f0
Ish J <ish@knights.ucf.edu>
2021-10-14 19:55:59 -0700

    Added 2 problems and added more work on site

---
+++ Trials/index2.js
@@ -0,0 +1,27 @@
+// You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+// Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+// Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+function fixTheMeerkat(arr) {
+    let temp = arr.reverse()
+    return temp
+}
+
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+function stray(numbers) {
+    let sorted = numbers.sort();
+    if (sorted[0] === sorted[1]) {
+      let answer = sorted[sorted.length -1]
+      return answer
+    }  else {
+      return sorted[0]
+    }
+      
+    }
