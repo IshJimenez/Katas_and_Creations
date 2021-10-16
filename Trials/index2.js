888d8c800734da5993a88010e776789daf1b1589
Ish J <ish@knights.ucf.edu>
2021-10-15 18:19:37 -0700

    Added problem and added more to site

--- Trials/index2.js
+++ Trials/index2.js
@@ -22,6 +22,12 @@ function stray(numbers) {
       return answer
     }  else {
       return sorted[0]
+    } 
     }
-      
-    }
+
+  // It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. 
+  // You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+  function removeChar(str){
+    return str.slice(1, str.length-1)
+   };
