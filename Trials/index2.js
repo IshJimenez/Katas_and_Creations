a140585b993777f82adbb037caf67223953d127a
Ish J <ish@knights.ucf.edu>
2021-10-16 11:28:51 -0700

    problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -31,3 +31,11 @@ function stray(numbers) {
   function removeChar(str){
     return str.slice(1, str.length-1)
    };
+
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+const areaOrPerimeter = function(l , w) {
+  if (l===w) return l*w
+  return 2 * (l+w)
+};
