8034dc464c76481a564bf629d6d8726dc3c61fd5
Ish J <ish@knights.ucf.edu>
2021-10-17 19:38:13 -0700

    problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -39,3 +39,26 @@ const areaOrPerimeter = function(l , w) {
   if (l===w) return l*w
   return 2 * (l+w)
 };
+
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+function switchItUp(number){
+  switch (number) {
+    case 0: return 'Zero';
+    case 1: return 'One';
+    case 2: return 'Two';
+    case 3: return 'Three';
+    case 4: return 'Four';
+    case 5: return 'Five';
+    case 6: return 'Six';
+    case 7: return 'Seven';
+    case 8: return 'Eight';
+    case 9: return 'Nine';
+  }
+}
