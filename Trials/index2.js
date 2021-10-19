32ab40df610a46736c31937d3b4fba43e574e7fa
Ish J <ish@knights.ucf.edu>
2021-10-18 23:59:57 -0700

    setting up for tomorrow

--- Trials/index2.js
+++ Trials/index2.js
@@ -1,3 +1,5 @@
+// Purpose of this file is to add a random problem per day.
+
 // You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
 
 // Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
@@ -62,3 +64,8 @@ function switchItUp(number){
     case 9: return 'Nine';
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 10 / 19 / 21
+
