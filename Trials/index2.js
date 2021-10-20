3397bc57a5b298a7acb368c60904f7dd7d3a4f83
Ish J <ish@knights.ucf.edu>
2021-10-19 23:42:36 -0700

    Problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -69,3 +69,19 @@ function switchItUp(number){
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // 10 / 19 / 21
 
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+function abbrevName(name){
+  let diffuse = name.split(" ");
+  let answer= (diffuse[0].charAt(0) + "." + diffuse[1].charAt(0)).toUpperCase();
+  return answer;
+
+}
