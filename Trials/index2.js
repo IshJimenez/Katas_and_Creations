ceb40f8d0956e82e090a74eb66cb0ded6e261604
Ish J <ish@knights.ucf.edu>
2021-10-26 22:50:47 -0700

    problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -138,3 +138,18 @@ function century(year) {
 function check(a,x){
   return a.includes(x);
 };
+
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+function updateLight(current) {
+switch(current) {
+  case 'green':
+    return 'yellow';
+  case 'yellow':
+    return 'red';
+  case 'red':
+    return 'green';
+}
+}
