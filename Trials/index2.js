499daf6f8c906a5ec17054c12f3024871b0014cc
Ish J <ish@knights.ucf.edu>
2021-11-01 23:36:58 -0700

    problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -227,3 +227,13 @@ function diffBig2(arr) {
     
     return largestNum - secondNum;
   }
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+  function boolToWord( bool ){
+    if (bool === true){
+      return "Yes"
+    } else {
+      return "No"
+    }
+  }
