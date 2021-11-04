544d887d2d25cb1ff6408e1a9118991cdc43be3c
Ish J <ish@knights.ucf.edu>
2021-11-03 22:02:53 -0700

    Problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -263,3 +263,21 @@ function diffBig2(arr) {
           return num;
       }
   }
+
+//   Very simple, given a number, find its opposite.
+
+//    Examples:
+//    1: -1
+//    14: -14
+//    -34: 34
+
+function opposite(number) {
+  if (number > 0 ) {
+    let b = number - (2 * number)
+    return b
+} else {
+  let a = number * -1
+  return a 
+}
+}
+
