ef903a8b132a0376aa0fe3bc550674b6bc70a609
Ish J <ish@knights.ucf.edu>
2021-11-13 15:50:28 -0800

    problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -409,3 +409,22 @@ function generateShape(int) {
 function square(number) {
   return Math.pow(2, number-1)
 }
+
+// Your task is to create functionisDivideBy (or is_divide_by) to check if an integer number is divisible by each out of two arguments.
+
+// A few cases:
+
+
+// (-12, 2, -6)  ->  true
+// (-12, 2, -5)  ->  false
+
+// (45, 1, 6)    ->  false
+// (45, 5, 15)   ->  true
+
+function isDivideBy(number, a, b) {
+  if (number % a === 0 && number % b === 0) {
+    return true 
+  } else {
+    return false 
+  }
+}
