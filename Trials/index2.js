fa7880e74cc9516c32c4b3fdc8534055b3994e65
Ish J <ish@knights.ucf.edu>
2021-11-12 18:02:38 -0800

    Solved a problem and continued work on recipe Website

--- Trials/index2.js
+++ Trials/index2.js
@@ -397,3 +397,15 @@ function findLongest(str) {
 function generateShape(int) {
   return Array(int).fill('+'.repeat(int)).join('\n')
 }
+
+// Write a program that calculates the number of grains of wheat on a specific square of chessboard given that the number on each square is double the previous one.
+
+// There are 64 squares on a chessboard.
+
+// #Example: square(1) = 1 square(2) = 2 square(3) = 4 square(4) = 8 etc...
+
+// Write a program that shows how many grains were on each square.
+
+function square(number) {
+  return Math.pow(2, number-1)
+}
