adc64571afc480d5c6b95043c626aece298b9af2
Ish J <ish@knights.ucf.edu>
2021-11-17 23:54:48 -0800

    Problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -453,3 +453,14 @@ function toNumberArray(stringarray){
 function move (position, roll) {
   return position + roll * 2
 }
+
+// Give you a function animal, accept 1 parameter:obj like this:
+
+// {name:"dog",legs:4,color:"white"}
+// and return a string like this:
+
+// "This white dog has 4 legs."
+
+function animal(obj){
+  return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs."
+}
