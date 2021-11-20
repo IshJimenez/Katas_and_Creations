c6cb88e442edc04fc4cacf25038eddd90b53bede
Ish J <ish@knights.ucf.edu>
2021-11-19 19:46:36 -0800

    Problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -474,3 +474,12 @@ function simpleMultiplication(number) {
     return number * 9
   }
 }
+
+// We need a function that can transform a number into a string.
+
+// Examples:
+// 123 --> "123"   ||   999 --> "999"
+
+function numberToString(num) {
+  return num.toString()
+}
