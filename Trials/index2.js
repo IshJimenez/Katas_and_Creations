9d5ea08bb290cc53fb689ddf61c4a84abf32bec4
Ish J <ish@knights.ucf.edu>
2021-11-20 16:54:45 -0800

    Problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -483,3 +483,26 @@ function simpleMultiplication(number) {
 function numberToString(num) {
   return num.toString()
 }
+
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+  let bm = weight / Math.pow(height, 2)
+  if (bm <= 18.5) {
+    return "Underweight"
+  } else if (bm <= 25.0) {
+    return "Normal"
+  } else if (bm <= 30.0) {
+    return "Overweight"
+  } else {
+    return "Obese"
+  }
+}
