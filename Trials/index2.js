99f41442ab1cc8bef7ebaca53c6cf5d03535ec80
Ish J <ish@knights.ucf.edu>
2021-11-21 19:41:52 -0800

    Problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -506,3 +506,43 @@ function bmi(weight, height) {
     return "Obese"
   }
 }
+
+// Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+// I love you
+// a little
+// a lot
+// passionately
+// madly
+// not at all
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+// Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+function howMuchILoveYou(nb_Petals) {
+  if (nb_Petals % 6 == 1) {
+    return "I love you"
+  } else if (nb_Petals % 6 == 2) {
+    return "a little"
+  } else if (nb_Petals % 6 == 3) {
+    return "a lot"
+  } else if (nb_Petals % 6 == 4) {
+    return "passionately"
+  } else if (nb_Petals % 6 == 5) {
+    return "madly"
+  } else {
+    return "not at all"
+  }
+}
+
+// function howMuchILoveYou(nbPetals) {
+//   let phrase = {    
+//     0: "not at all",
+//     1: "I love you",
+//     2: "a little",
+//     3: "a lot",
+//     4: "passionately",
+//     5: "madly"
+//   }
+//   return phrase[nbPetals%6]
+// }
