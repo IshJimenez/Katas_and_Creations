9e2b9f47fc1aa7eea643c5ae1c3675e6fa7bece4
Ish J <ish@knights.ucf.edu>
2021-11-25 23:37:14 -0800

    Problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -598,3 +598,13 @@ function peopleWithAgeDrink(old) {
 function twiceAsOld(dadYearsOld, sonYearsOld) {
   return Math.abs(dadYearsOld - (sonYearsOld*2));
 }
+
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+function otherAngle(a, b) {
+  return 180 - a - b
+}
