37a728d2d98fb9bf7e96ccb1fe95c9e257a2d320
Ish J <ish@knights.ucf.edu>
2021-12-06 23:45:22 -0800

    Problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -775,3 +775,11 @@ function countSheeps(arrayOfSheep) {
       
   return num;
 }
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+
+function getVolumeOfCuboid(l, w, h){
+  return l * w * h
+}
+
+
