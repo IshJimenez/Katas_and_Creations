63b2a28a4bcf61e6402b20893b35ce00fbdce887
Ish J <ish@knights.ucf.edu>
2021-12-09 23:52:55 -0800

    Problem for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -806,3 +806,25 @@ function between(a, b) {
   }
   return arr
 }
+
+// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+// Your function will be tested with pre-made examples as well as random ones.
+
+// If you can, try writing it in one line of code.
+
+function findDifference(a, b) {
+  var num1 = a[0];
+  var num2 = b[0];
+  for (var i = 1; i < 3; i++) {
+    num1 = num1 * a[i];
+    num2 = num2 * b[i];
+  }
+  
+  if (num1 > num2) {
+    return num1 - num2
+  }
+  return num2 - num1;
+}
