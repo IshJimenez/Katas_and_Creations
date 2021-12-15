84c553feb3cd401201ab883cf188807e8ba3e79d
Ish J <ish@knights.ucf.edu>
2021-12-14 18:56:32 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -828,3 +828,42 @@ function findDifference(a, b) {
   }
   return num2 - num1;
 }
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+function past(h, m, s){
+  let a = h * 3600000
+  let b = m * 60000
+  let c = s * 1000
+  return a + b + c
+   }
+
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+  if(flower1 % 2 == 0 && flower2 % 2 == 1){
+   return true;
+}
+if(flower1 % 2 == 1 && flower2 % 2 == 0){
+  return true;
+}
+else{
+   return false;
+   }
+}
