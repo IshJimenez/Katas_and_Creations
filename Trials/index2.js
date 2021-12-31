5f3c3022e8982ff520555092f299f501f7534b6c
Ish J <ish@knights.ucf.edu>
2021-12-30 20:54:37 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -968,3 +968,24 @@ function pipeFix(numbers){
 let a = "code";
 let b = "wa.rs";
 let name = a + b;
+
+
+// Wilson primes satisfy the following condition. Let P represent a prime number.
+
+// Then ((P-1)! + 1) / (P * P) should give a whole number.
+
+// Your task is to create a function that returns true if the given number is a Wilson prime.
+
+function amIWilson(p) {
+  let factorial = p - 1;
+  for (i = factorial; i > 1; i--) {
+    factorial = factorial * (i - 1)
+  }
+  let wilson = (factorial + 1) / (p * p);
+
+  if (wilson % 1 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
