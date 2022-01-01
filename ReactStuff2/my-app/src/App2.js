20e2b1808513aedd8ab52da49f4a67217a902f4b
Ish J <ish@knights.ucf.edu>
2021-12-31 16:42:03 -0800

    React recap

---
+++ ReactStuff2/my-app/src/App2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+function PracticePage(){
+    return (
+        <div>
+            <h1>Hey</h1>
+            <h1>Look Awesome Facts</h1>
+            <ul>
+                <li>Venus is the hottest planet in solar system</li>
+                <li>Mars is the second smallest planet in the solar system after Mercury </li>
+                <li>Mars has 2 moons called Deimos and Phobos</li>
+            </ul>
+        </div>
+    );
+}
+
+export default PracticePage;
