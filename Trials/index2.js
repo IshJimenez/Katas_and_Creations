1c3b3090eea077c8ddddc7f4fb23e05754a25d8c
Ish J <ish@knights.ucf.edu>
2022-01-03 20:50:06 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -1030,3 +1030,10 @@ function getGrade (s1, s2, s3) {
     return "F"
   }
 }
+
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+const smash = words => words.join(' ');
