eabc103511a7699bb9b291772c562544b6a04a5d
Ish J <ish@knights.ucf.edu>
2022-01-04 20:50:14 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -1037,3 +1037,16 @@ function getGrade (s1, s2, s3) {
 // ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 
 const smash = words => words.join(' ');
+
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x){
+  let y = x.filter(el => el == 'good').length;
+  if ( y > 0 && y < 3){
+  return 'Publish!';
+  } else if (y > 2){
+  return 'I smell a series!';
+  }else {return 'Fail!';}
+  };
