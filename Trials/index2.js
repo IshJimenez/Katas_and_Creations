8551fc498d6a78a936155381f6bea5cc7d526e70
Ish J <ish@knights.ucf.edu>
2022-01-16 15:59:56 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -1183,3 +1183,30 @@ function squareSum(numbers) {
   }
   return sum
 }
+
+// A magician in the subway showed you a trick, he put an ice brick in a bottle to impress you. The brick's length and width are equal, forming a square; its height may be different. Just for fun and also to impress the magician and people around, you decided to calculate the brick's volume. Write a function iceBrickVolume that will accept these parameters:
+
+// radius - bottle's radius (always > 0);
+// bottleLength - total bottle length (always > 0);
+// rimLength - length from bottle top to brick (always < bottleLength);
+// And return volume of ice brick that magician managed to put into a bottle.
+
+// All inputs are integers. Assume no irregularities to the cuboid brick. You may assume the bottle is shaped like a cylinder. The brick cannot fit inside the rim.
+
+// All inputs are integers.
+
+// Example 1:
+// radius = 1
+// bottle_length = 10
+// rim_length = 2
+
+// volume = 16
+
+function iceBrickVolume(radius, bottleLength, rimLength) {
+  let brickL = bottleLength - rimLength;
+  let brickD = radius*2;
+  let brickH = radius; 
+  
+  return brickL * brickD * brickH;
+  
+  }
