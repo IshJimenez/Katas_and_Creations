34ee1ec3733f98e52d3c47dc6c9451cfdd59c3bd
Ish J <ish@knights.ucf.edu>
2022-01-17 18:39:36 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -1210,3 +1210,17 @@ function iceBrickVolume(radius, bottleLength, rimLength) {
   return brickL * brickD * brickH;
   
   }
+
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+
+function remove(s){
+  return s.replace(/!*$/g,'');
+}
