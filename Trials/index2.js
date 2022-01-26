de4ac579076b61507ee2c82c54f081b2e12612ec
Ish J <ish@knights.ucf.edu>
2022-01-25 20:46:36 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -1288,3 +1288,16 @@ function find_average(array) {
  function greetings(name){
    return `Hello, ${name} how are you doing today?`;
  }
+
+//  There are pillars near the road. The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+
+//    number of pillars (≥ 1);
+//    distance between pillars (10 - 30 meters);
+//    width of the pillar (10 - 50 centimeters).
+// Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
+
+ function pillars(num_pill, dist, width) {
+  let distance = dist * ((num_pill - 1) * 100)
+  let pillars = num_pill - 2
+  return distance + (width * pillars)
+}
