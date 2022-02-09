5140e2ded4fccd4ee14444bd0a4b55d282450464
Ish J <ish@knights.ucf.edu>
2022-02-08 20:57:05 -0800

    Editing navBar

--- albaD/app.js
+++ albaD/app.js
@@ -1,14 +1,15 @@
 document.addEventListener("click", e => {
     const isDropdownButton = e.target.matches("[data-dropdownButton]")
-})
+    if (!isDropdownButton && e.target.closet("[data-dropdown]") != null) return
 
 let currentDropdown
 if (isDropdownButton) {
-    currentDropdown = e.target.closet('[data-dropdown]')
-    currentDropdown.classList.toggle('active')
+    currentDropdown = e.target.closet("[data-dropdown]")
+    currentDropdown.classList.toggle("active")
 }
 
 document.querySelectorAll("[data-dropdown].active").forEach(dropdown =>{
     if (dropdown === currentDropdown) return
-    dropdown.classList.remove('active')
+    dropdown.classList.remove("active")
+})
 })
