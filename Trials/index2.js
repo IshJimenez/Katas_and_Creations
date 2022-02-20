367962cb1289dbe2c295de15aacf1e00075ce5f9
Ish J <ish@knights.ucf.edu>
2022-02-19 16:24:50 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -1462,3 +1462,9 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     return false
   }
 };
+
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+function isPalindrome(x) {
+  return x.toLowerCase().split('').reverse().join('') == x.toLowerCase()
+}
