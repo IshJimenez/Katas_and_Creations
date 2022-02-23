5d7f97fb6db961fb81f9e50dc04138ccd31546ac
Ish J <ish@knights.ucf.edu>
2022-02-22 23:46:11 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -1493,3 +1493,11 @@ let summation = function (num) {
   }
   return sum
 }
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+  return s.replace(/!/g,'');
+}
+
+
