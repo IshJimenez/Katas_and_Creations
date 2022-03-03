c57e2f5f70ef5ead3ba1cc354585dd208e8f9b0d
Ish J <ish@knights.ucf.edu>
2022-03-02 23:16:15 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -1506,3 +1506,10 @@ function removeExclamationMarks(s) {
 function seconds(m) {
   return m * 60
 }
+
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key country
+
+function myFunction(obj){
+  return obj.country;
+}
