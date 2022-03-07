8c22e95eb32129667f08afb58237207c903fae83
Ish J <ish@knights.ucf.edu>
2022-03-06 19:41:04 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -1525,3 +1525,9 @@ function addition( a, b ) {
 function addition(num) {
 	return ++num;
 }
+
+// Create a function that takes an array containing only numbers and return the first element.
+
+function getFirstValue(arr) {
+	return arr[0];
+}
