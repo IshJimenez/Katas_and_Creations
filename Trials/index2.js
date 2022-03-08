cd4acd9eea1a6e52c9f7f62e27589954ffa42595
Ish J <ish@knights.ucf.edu>
2022-03-07 21:09:05 -0800

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -1531,3 +1531,9 @@ function addition(num) {
 function getFirstValue(arr) {
 	return arr[0];
 }
+
+// Write a function that takes the base and height of a triangle and return its area.
+
+function triArea(base, height) {
+	return base * height /2
+}
