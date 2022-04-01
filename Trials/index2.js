4f5c846d8bb1a81d5c4c95a2d7ea7f337a04faf0
Ish J <ish@knights.ucf.edu>
2022-04-01 16:55:53 -0700

    Prob for today

--- Trials/index2.js
+++ Trials/index2.js
@@ -1572,6 +1572,17 @@ function answer2(){
   }
 }
 
+// Calculate the sum of odd numbers greater than 10 and less than 30
+
+let sum = 0;
+
+for(let i = 11; i <= 30; i += 2)
+{
+    sum += i;
+    console.log(sum)
+}
+
+
 // Write a function that accepts an array of strings. Return the longest string.
 
 function longestString1(arr) {
