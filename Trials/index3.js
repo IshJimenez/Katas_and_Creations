ce1226f85d99417dfeae2910a0b53f028eded208
IshJimenez <ish@knights.ucf.edu>
2022-05-29 16:15:02 -0700

    Prob for today

--- Trials/index3.js
+++ Trials/index3.js
@@ -22,3 +22,16 @@ function fizzbuzz(n){
 }
 }
 fizzbuzz(30);
+
+// Create a function that takes voltage and current and returns the calculated power.
+
+// Examples
+// circuitPower(230, 10) ➞ 2300
+
+// circuitPower(110, 3) ➞ 330
+
+// circuitPower(480, 20) ➞ 9600
+
+fucntion answer(voltage, current) {
+    return voltage * current
+}
