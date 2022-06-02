eb47324562bb065405d30fff041da85fc4acbb51
IshJimenez <ish@knights.ucf.edu>
2022-06-01 17:52:16 -0700

    Started the node folder

---
+++ tuto1/1intro.js
@@ -0,0 +1,15 @@
+const amount = 21
+
+if (amount < 10){
+    console.log("Small digits")
+}
+else {
+    console.log("large digits")
+}
+
+console.log('Hey look at me on  node !')
+
+// console.log(_dirname)
+setInterval(() => {
+    console.log('boo')
+}, 1000)
---
+++ tuto1/app.js
@@ -0,0 +1,7 @@
+// Modules
+
+const sayHi = (name) =>{
+    console.log(`Hello person ${name}`)
+}
+
+sayHi('Bob')
