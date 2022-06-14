c1c949a3d35107b100f7a086594ae601a6b850ea
IshJimenez <ish@knights.ucf.edu>
2022-06-13 20:56:14 -0700

    Added to node folder

--- tuto1/app.js
+++ tuto1/app.js
@@ -11,7 +11,7 @@ const names = require('./4name');
 const sayHi = require('./5utlis')
 const info = require('./6alternativefla')
 // console.log(names)
-
+require('./7mindgrenade')
 sayHi('Bob')
 sayHi(names.mario)
 sayHi(names.luigi)
