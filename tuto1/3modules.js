6eae4986f8965242952afc0f0ab5192981b59b83
IshJimenez <ish@knights.ucf.edu>
2022-06-29 20:35:10 -0700

    Added files to node tuto

---
+++ tuto1/3modules.js
@@ -0,0 +1,8 @@
+const names = require('./4name');
+const sayHi = require('./5utlis')
+const info = require('./6alternativefla')
+// console.log(names)
+require('./7mindgrenade')
+sayHi('Bob')
+sayHi(names.mario)
+sayHi(names.luigi)
--- tuto1/app.js
+++ tuto1/app.js
@@ -1,17 +1,2 @@
-// Modules
-// const secret = 'superSECRET'
-// const mario = 'mario'
-// const luigi = 'luigi'
-
-// const sayHi = (name) =>{
-//     console.log(`Hello person ${name}`)
-// }
-
-const names = require('./4name');
-const sayHi = require('./5utlis')
-const info = require('./6alternativefla')
-// console.log(names)
-require('./7mindgrenade')
-sayHi('Bob')
-sayHi(names.mario)
-sayHi(names.luigi)
+const {readFileSync} = require('fs')
+const fs = require
