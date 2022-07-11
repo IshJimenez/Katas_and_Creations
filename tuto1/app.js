14dc3cedfe57ef266d829a0227a4f51e29ec5272
IshJimenez <ish@knights.ucf.edu>
2022-07-10 20:35:58 -0700

    Added more node files

--- tuto1/app.js
+++ tuto1/app.js
@@ -1,9 +1,15 @@
-const http = require('http')
+let http = require('http')
+let fs = require('fs')
 
-const server = http.createServer()
-
-server.on('request', (req, res) => {
-    res.end('Bonjour')
+http.createServer(function (req,res){
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
+    fileStream.on('open', ()=> {
+fileStream.pipe(res)
+    })
+    fileStream.on('error', (err)=> {
+        res.end(err)
+    })
 })
-
-server.listen(5000)
+.listen(5000)
