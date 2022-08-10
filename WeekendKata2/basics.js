01072f4841b820e4a9f92e36fe929872ddbf194d
IshJimenez <ish@knights.ucf.edu>
2022-08-09 20:40:46 -0700

    Prob for today

--- WeekendKata2/basics.js
+++ WeekendKata2/basics.js
@@ -100,3 +100,20 @@ newColor = ["Red", "Green", "White", "Black"];
 console.log(newColor.toString());
 console.log(newColor.join());
 console.log(newColor.join('+'));
+
+
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+
+function myFunction(a, b) {
+  if (a == b & a === b) {
+  return true
+  } else {
+  return false
+  }
+}
+
+
+// function myFunction(a, b) {
+//   return a === b;
+// }
