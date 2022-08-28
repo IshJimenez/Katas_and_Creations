768dca3238ff6abe850c1d96a4eb0731e504daa9
IshJimenez <ish@knights.ucf.edu>
2022-08-28 14:43:44 -0700

    Kata for today

--- WeekendKata2/basics.js
+++ WeekendKata2/basics.js
@@ -140,3 +140,11 @@ function myFunction(a) {
 function myFunction (a, b) {
 return a + b
 }
+
+// Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
+// Return the result
+
+function myFunction(a) {
+  return a.slice(0, 3);
+}
