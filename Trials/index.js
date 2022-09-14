c32ae288b70a8c444fc3ea8db6e664492a11ae75
IshJimenez <ish@knights.ucf.edu>
2022-09-14 16:27:12 -0700

    Prob for today

--- Trials/index.js
+++ Trials/index.js
@@ -89,3 +89,18 @@ function smallestMulti(n){
 
 smallestMulti(5)
 
+// Return the result// Write a function that takes an array (a) as argument
+// Remove the first 3 elements of 'a'
+// Return the result
+
+function myFUnction(a){
+    return a.slice(3);
+}
+
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+
+function myFunction (str){
+return  str.slice(-3);
+}
