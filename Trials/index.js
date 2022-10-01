a0de968864d439ef0a142b9ff7c4e610ce641fb4
IshJimenez <ish@knights.ucf.edu>
2022-09-30 17:43:31 -0700

    Kata for today

--- Trials/index.js
+++ Trials/index.js
@@ -113,3 +113,12 @@ return  str.slice(-3);
 function myFunction(a) {
     return { key : a };
  }
+
+// Write a function that takes two strings (a and b) as arguments
+// Create an object that has a property with key 'a' and a value of 'b'
+// Return the object
+
+ function myFunction(a, b) {
+   
+    return {[a]:b}
+ }
