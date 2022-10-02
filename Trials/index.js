2bae276795de402f5c2d22deffd8c6bfc5bad14a
IshJimenez <ish@knights.ucf.edu>
2022-10-02 16:20:21 -0700

    Prob for today

--- Trials/index.js
+++ Trials/index.js
@@ -136,3 +136,11 @@ function myFunction(a) {
     }
     return app
  }
+
+
+// Write a function that takes an object with two properties and a string as arguments
+// It should return the value of the property with key equal to the value of the string
+
+ function myFunction(obj, key) {
+    return obj[key]
+  }
