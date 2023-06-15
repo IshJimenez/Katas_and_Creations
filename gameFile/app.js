68694b6a157571976dc08c438996a23c851766f9
ish <ish@knights.ucf.edu>
2023-06-14 22:35:24 -0700

    Added some more logic now you get results based off the comp and user choice

--- gameFile/app.js
+++ gameFile/app.js
@@ -2,25 +2,48 @@ const enemyChoiceDisplay = document.getElementById('enemyChoice')
 const userChoiceDisplay = document.getElementById('yourChoice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
-let userChoice
+let youtChoice
 let enemeyChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     youtChoice = e.target.id
     userChoiceDisplay.innerHTML = youtChoice
     generateEnenmyChoice()
+    getResult()
 }))
 function generateEnenmyChoice(){
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length ) + 1
+    const randomNumber = Math.floor(Math.random() * 3 ) + 1
     
     if (randomNumber === 1) {
-        enemeyChoice = 'rock'
+        enemeyChoice = 'Rock'
     }
     if (randomNumber === 2) {
-        enemeyChoice = 'kami'
+        enemeyChoice = 'Kami'
     }
     if (randomNumber === 3) {
-        enemeyChoice = 'tijieras'
+        enemeyChoice = 'Tijeras'
     }
     enemyChoiceDisplay.innerHTML = enemeyChoice
+}
+function getResult(){
+    if (enemeyChoice === youtChoice){
+        result = "TIE"
+    }
+    if (enemeyChoice === 'Rock' && youtChoice === 'Kami'){
+        result = "YOU ARE A WINNER"
+    }
+    if (enemeyChoice === 'Rock' && youtChoice === 'Tijeras'){
+        result = "DAM GOT BEAT BY THE MACHINE"
+    }
+    if (enemeyChoice === 'Kami' && youtChoice === 'Tijeras'){
+        result = "YOU WON"
+    }
+    if (enemeyChoice === 'Kami' && youtChoice === 'Rock'){
+        result = "THR MACHINE HAS WON"
+    }
+    if (enemeyChoice === 'Tijeras' && youtChoice === 'Kami'){
+        result = "YOU LOSE REALLY WOW"
+    }
+    resultDisplay.innerHTML = result
 }
