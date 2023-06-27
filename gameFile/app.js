6c5d59791fac287c9d3d67bb3188630368fcc610
ish <ish@knights.ucf.edu>
2023-06-26 18:11:38 -0700

    Updated colors and text

--- gameFile/app.js
+++ gameFile/app.js
@@ -16,34 +16,37 @@ function generateEnenmyChoice(){
     const randomNumber = Math.floor(Math.random() * possibleChoices.length ) + 1
     
     if (randomNumber === 1) {
-        enemeyChoice = 'Rock'
+        enemeyChoice = 'ROCK'
     }
     if (randomNumber === 2) {
-        enemeyChoice = 'Kami'
+        enemeyChoice = 'KAMI'
     }
     if (randomNumber === 3) {
-        enemeyChoice = 'Tijeras'
+        enemeyChoice = 'TIJERAS'
     }
     enemyChoiceDisplay.innerHTML = enemeyChoice
 }
 function getResult(){
     if (enemeyChoice === youtChoice){
-        result = "TIE Try Again To Beat The Machine"
+        result = "TIE But Its More Like A Loss :-)"
     }
-    if (enemeyChoice === 'Rock' && youtChoice === 'Kami'){
-        result = "YOU ARE A WINNER"
+    if (enemeyChoice === 'ROCK' && youtChoice === 'KAMI'){
+        result = "You Won Wow Think You're So Cool But Can You Win Again"
     }
-    if (enemeyChoice === 'Rock' && youtChoice === 'Tijeras'){
-        result = "DAM GOT BEAT BY THE MACHINE"
+    if (enemeyChoice === 'ROCK' && youtChoice === 'TIJERAS'){
+        result = "Dam You Got Beat Bad Sorry Not Sorry"
     }
-    if (enemeyChoice === 'Kami' && youtChoice === 'Tijeras'){
-        result = "YOU WON"
+    if (enemeyChoice === 'KAMI' && youtChoice === 'TIJERAS'){
+        result = "You Won Bet You Cheated Though So"
     }
-    if (enemeyChoice === 'Kami' && youtChoice === 'Rock'){
-        result = "THE MACHINE HAS WON"
+    if (enemeyChoice === 'KAMI' && youtChoice === 'ROCK'){
+        result = "I Have Won And You Have Not HaHaHa Loserrr"
     }
-    if (enemeyChoice === 'Tijeras' && youtChoice === 'Kami'){
-        result = "YOU LOSE REALLY WOW"
+    if (enemeyChoice === 'TIJERAS' && youtChoice === 'KAMI'){
+        result = "You LOST You LOSER Yeah I Won In Your Face"
+    }
+    if (enemeyChoice === 'TIJERAS' && youtChoice === 'ROCK'){
+        result = "Alas You Have Won But Remember Cheaters Dont Prosper Sooo"
     }
     resultDisplay.innerHTML = result
 }
